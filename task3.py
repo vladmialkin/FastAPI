e0dd52c3f4e9dd6e89564aa5104d2635407ea94a
programmer <myalkin.vlad@yandex.ru>
2024-02-02 00:50:34 +0300

    test

--- task3.py
+++ task3.py
@@ -1,5 +1,11 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://postgres:919@localhost/fast_api_bd", echo=True,)
+engine = create_engine("postgresql+psycopg2://postgres:919@localhost/postgres", echo=True,)
 engine.connect()
+
+
+
+
+
+
 
