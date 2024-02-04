397155518b9cc93b2eb3b31aa5d7b50d7ee1d2d0
programmer <myalkin.vlad@yandex.ru>
2024-02-04 16:43:36 +0300

    fix database connection

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from app.models.core import metadata
+from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of .env,
 # can be acquired:
