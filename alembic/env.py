12fc80316667f5a1c3b1f5f7bf47cc79b338972a
mialkin <mialkin@bolid.ru>
2024-02-08 17:24:23 +0300

    fix: Fix table creation with alembic.

--- alembic/env.py
+++ alembic/env.py
@@ -4,10 +4,19 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from app.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from app.db.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, "DB_HOST", DB_HOST)
+config.set_section_option(section, "DB_NAME", DB_NAME)
+config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_USER", DB_USER)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -16,14 +25,11 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.db.models import Base
-
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-
-# target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
+# other values from the config, defined by the needs of .env,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
